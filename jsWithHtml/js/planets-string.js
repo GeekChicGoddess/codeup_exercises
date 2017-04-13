1074ad189cfeb417d83d930f7514020e7d3472f4
melodytempleton <mtempletoncantu@gmail.com>
2017-04-13 15:01:33 -0500

    I put my cool card game here

--- jsWithHtml/js/planets-string.js
+++ jsWithHtml/js/planets-string.js
@@ -104,3 +104,40 @@
     }
 twentyFourToTwelve("00:00");
 })();
+
+
+
+var suits = ["Ace of Hearts", "Ace of Diamonds", "Ace of Spades", "Ace of Clubs"];
+// declare and initialize array
+var suitsOrderList = "";
+suits.forEach(function (element, index, array) {
+    // element is the suit name..index is the iterator..array is the suits array itself
+     suitsOrderList += 'The card at ' + index + ' is: ' + element + ".\n";
+// creating a list that tells us the order of the cards in the array
+});
+alert(suitsOrderList);
+var newSuitsOrder = []; // going to put them in a new, secret order here
+var numberIndicesLeft = 3; // there are 4 (0,1, 2, 3) items in suits
+var randomCardOrderer = 0;  // which card are we pulling off the array?
+var newSuitsOrderList = ""; // this will be the new list of cards that displays
+alert("I'm mixing up the cards now"); // going to mix them up below
+for (var i = 0; i < 4; i++){
+    randomCardOrderer = Math.floor(Math.random() * numberIndicesLeft); // which card do we pick?
+    newSuitsOrder.push(suits[randomCardOrderer]); // push the random card onto new array
+    suits.splice(randomCardOrderer, 1); // remove that card from the orginal suit array
+    numberIndicesLeft -= i; // how many items left in suits?
+}
+var spade = newSuitsOrder.indexOf("Ace of Spades"); // where is the Ace of Spades?
+var guess = prompt("Which index do you think the Ace of Spades is at?");
+if (spade == guess) { // is your guess right?
+    alert("Yes! win!");
+}
+else{ // guess not
+    alert("Sorry. You lose");
+};
+alert("Here is the new order of my array..." + newSuitsOrderList);
+newSuitsOrder.forEach(function (element, index, array) { // loop through and list them all
+    newSuitsOrderList += 'The card at ' + index + ' is: ' + element + ".\n";
+// element is the suit name..index is the iterator.
+});
+alert(newSuitsOrderList); // Here is the new order .....honest
