2ecf31409f04dc35518acc40afda8ab70408d6bf
melodytempleton <mtempletoncantu@gmail.com>
2017-04-14 11:31:39 -0500

    circle exercise

---
+++ jsWithHtml/js/circles.js
@@ -0,0 +1,47 @@
+/**
+ * Created by melodytempleton on 4/14/17.
+ */
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            var area = 3.14 * Math.pow(this.radius, 2);
+            return area;
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            var areaLog = 0;
+
+            if(doRounding){
+                areaLog = Math.round(this.getArea());
+            }
+            else {
+                areaLog = this.getArea();
+            }
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + areaLog);
+        }
+    };
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
